f63040f2de7289cb299feff388b2570e2993e8aa
Mateusz Serek <mateuszserek@MacBook-Pro-Mateusz.local>
2025-05-08 11:25:27 +0200

    prepare html elements and js class

---
+++ script.js
@@ -0,0 +1,12 @@
+notDoneToDoArea = document.querySelector("#not-done-todo")
+doneToDoArea = document.querySelector("#done-todo")
+
+class ToDo {
+    constructor(text) {
+        this.text = text
+    }
+
+    generateElements = () => {
+        return 
+    }
+}
