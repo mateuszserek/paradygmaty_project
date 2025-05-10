5f1dae5269dd7a85aa412c8758f1930b8f3a81a7
Mateusz Serek <mateuszserek@MacBook-Pro-Mateusz.local>
2025-05-10 14:55:44 +0200

    major JS  mechanics and some css style

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
-notDoneToDoArea = document.querySelector("#not-done-todo")
-doneToDoArea = document.querySelector("#done-todo")
+"use strict";
+
+const notDoneToDoArea = document.querySelector("#not-done-todo")
+const doneToDoArea = document.querySelector("#done-todo")
+const addButton = document.querySelector("#add-todo-button")
+const textInput = document.querySelector("#todo-text-input")
+
 
 class ToDo {
     constructor(text) {
         this.text = text
+        this.doneButton = document.createElement("button")
+        this.deleteButton = document.createElement("button")
+        this.todoDiv = document.createElement("div")
+        this.pElem = document.createElement("p")
+
+        this.generateElements(this.doneButton, this.deleteButton, this.todoDiv, this.pElem)
+        this.eventListeners()
     }
 
-    generateElements = () => {
-        return 
+    generateElements = (doneB, deleteB, todoDiv, pElem) => {
+        todoDiv.classList.add("todo") 
+
+        pElem.textContent = this.text
+        doneB.type = "submit"
+        doneB.textContent = "Ukończono"
+        deleteB.type = "submit"
+        deleteB.textContent = "Usuń"
+
+        todoDiv.append(pElem)
+        todoDiv.append(doneB)
+        todoDiv.append(deleteB)
+
+        notDoneToDoArea.append(todoDiv)
+    }
+
+    eventListeners = () => {
+        this.deleteButton.addEventListener("click", () => {
+            this.killElement()
+        })
+
+        this.doneButton.addEventListener("click", () => {
+            doneToDoArea.appendChild(this.todoDiv)
+        })
+    }
+
+    killElement = () => {
+        this.todoDiv.remove()
+        delete this
     }
 }
+
+
+addButton.addEventListener("click", () => {
+    let todoString = textInput.value
+    textInput.value = ""
+    // function in prolog to check todo string
+    const todo = new ToDo(todoString)
+})
